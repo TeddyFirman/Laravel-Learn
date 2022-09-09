044980a7ddc5361327ee920f889ec670947b3fbd
Teddy Firman <teddyfirman902@gmail.com>
2022-09-09 11:12:32 +0700

    added array post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,4 +2,11 @@
 
 @section('container')
     <h1>Halaman Blog</h1>
+    @foreach ($posts as $post)
+    <article class="mb-5">
+        <h2>{{ $post['title'] }}</h2>
+        <h5>By: {{ $post['author'] }}</h5>
+        <p>{{ $post['body'] }}</p>
+    </article>
+    @endforeach
 @endsection

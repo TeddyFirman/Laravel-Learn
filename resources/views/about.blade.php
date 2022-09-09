bd9da406fb3be7b0e8369dbe3af993cef88307ee
Teddy Firman <teddyfirman902@gmail.com>
2022-09-09 10:23:03 +0700

    added blade formatter

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,12 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Tedd Blog | About</title>
 </head>
+
 <body>
     <h1>Halaman About</h1>
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
     <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
 </body>
+
 </html>

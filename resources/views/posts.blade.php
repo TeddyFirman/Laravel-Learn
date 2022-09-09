5bdce6f5026ca1578df6f8e2c33208f9ad021ce9
Teddy Firman <teddyfirman902@gmail.com>
2022-09-09 10:41:56 +0700

    added extends

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Tedd Blog | Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+    <h1>Halaman Blog</h1>
+@endsection

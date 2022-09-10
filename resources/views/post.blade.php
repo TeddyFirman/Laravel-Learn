c3f536b140b21a50553e5b667575e6f2cb5bcb34
Teddy Firman <teddyfirman902@gmail.com>
2022-09-11 02:48:04 +0700

    added eloquent laravel first category foreign key post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+        <p>By. Tedd404 in {{ $post->category->name }}</p>
         <h5>{{ $post->author }}</h5>
         {!! $post->body !!}
     </article>

a2a1c395f7fd994fd8fe3192fe3fbe62980c8f9a
Teddy Firman <teddyfirman902@gmail.com>
2022-09-11 03:12:35 +0700

    added eloquent laravel categories relation and navbar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 
@@ -36,3 +37,18 @@ Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function () {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});

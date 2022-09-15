46fe8050c37db78723991c1c7e8697a6fb1623f4
Teddy Firman <teddyfirman902@gmail.com>
2022-09-15 08:19:16 +0700

    learn seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,54 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Tedd404',
+            'email' => 'tedd404@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Tedd666',
+            'email' => 'tedd666@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Website',
+            'slug' => 'website'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ut quos numquam eius animi laboriosam adipisci nesciunt asperiores',
+            'body' => '<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, animi dignissimos. Reprehenderit sed tempore commodi officia perspiciatis, illum necessitatibus, eveniet veritatis obcaecati ipsum iusto aut autem! Tenetur sit placeat aliquam aliquid.</p> <p> Culpa quia cumque repudiandae! Tempora veniam sequi explicabo. Nisi sed odio repellat adipisci? Nam ratione odio dolorem natus adipisci dicta quasi facilis, perspiciatis itaque praesentium pariatur ipsum tenetur illo nisi nesciunt hic sed? Explicabo </p> <p> voluptates magnam accusantium incidunt nulla blanditiis amet natus esse assumenda! Praesentium, enim officiis. Mollitia ratione vitae dolorum veniam eligendi, libero fugiat perspiciatis iure, eveniet pariatur natus, nemo provident temporibus soluta? Necessitatibus optio ex aliquid inventore!</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ut quos numquam eius animi laboriosam adipisci nesciunt asperiores',
+            'body' => '<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, animi dignissimos. Reprehenderit sed tempore commodi officia perspiciatis, illum necessitatibus, eveniet veritatis obcaecati ipsum iusto aut autem! Tenetur sit placeat aliquam aliquid.</p> <p> Culpa quia cumque repudiandae! Tempora veniam sequi explicabo. Nisi sed odio repellat adipisci? Nam ratione odio dolorem natus adipisci dicta quasi facilis, perspiciatis itaque praesentium pariatur ipsum tenetur illo nisi nesciunt hic sed? Explicabo </p> <p> voluptates magnam accusantium incidunt nulla blanditiis amet natus esse assumenda! Praesentium, enim officiis. Mollitia ratione vitae dolorum veniam eligendi, libero fugiat perspiciatis iure, eveniet pariatur natus, nemo provident temporibus soluta? Necessitatibus optio ex aliquid inventore!</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ut quos numquam eius animi laboriosam adipisci nesciunt asperiores',
+            'body' => '<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, animi dignissimos. Reprehenderit sed tempore commodi officia perspiciatis, illum necessitatibus, eveniet veritatis obcaecati ipsum iusto aut autem! Tenetur sit placeat aliquam aliquid.</p> <p> Culpa quia cumque repudiandae! Tempora veniam sequi explicabo. Nisi sed odio repellat adipisci? Nam ratione odio dolorem natus adipisci dicta quasi facilis, perspiciatis itaque praesentium pariatur ipsum tenetur illo nisi nesciunt hic sed? Explicabo </p> <p> voluptates magnam accusantium incidunt nulla blanditiis amet natus esse assumenda! Praesentium, enim officiis. Mollitia ratione vitae dolorum veniam eligendi, libero fugiat perspiciatis iure, eveniet pariatur natus, nemo provident temporibus soluta? Necessitatibus optio ex aliquid inventore!</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,7 @@
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
 
-            <p>By. Tedd404 in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a> </p>
+            <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a> </p>
 
             <p>{{ $post->excerpt }}</p>
 

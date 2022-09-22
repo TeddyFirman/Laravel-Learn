0afb17d34f26c8c62c548a69d54891553761d79e
Teddy Firman <teddyfirman902@gmail.com>
2022-09-22 22:23:42 +0700

    redesign for all posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,9 +5,11 @@
 
     @if ($posts->count())
         <div class="card mb-3">
-            <img class="card-img-top" src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" alt="Card image cap">
+            <img class="card-img-top" src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}"
+                alt="{{ $posts[0]->category->name }}">
             <div class="card-body text-center">
-                <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+                <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
                 <p>
                     <small class="text-muted">By. <a href="/authors/{{ $posts[0]->author->username }}"
                             class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
@@ -24,7 +26,32 @@
         <p class="text-center fs-4">No posts found!</p>
     @endif
 
-    @foreach ($posts as $post)
+    <div class="container">
+        <div class="row">
+            @foreach ($posts->skip(1) as $post)
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" src="https://source.unsplash.com/500x400?{{ $post->category->name }}"
+                            alt="{{ $post->category->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p>
+                                <small class="text-muted">By. <a href="/authors/{{ $post->author->username }}"
+                                        class="text-decoration-none">{{ $post->author->name }}</a> in <a
+                                        href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+                                        {{ $post->category->name }}</a>
+                                    {{ $post->created_at->diffForHumans() }}</small>
+                            </p>
+                            <p class="card-text">{{ $post->excerpt }}</p>
+                            <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more..</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    {{-- @foreach ($posts->skip(1) as $post)
         <article class="mb-5 border-bottom pb-3">
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
@@ -39,5 +66,5 @@
 
             <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
         </article>
-    @endforeach
+    @endforeach --}}
 @endsection

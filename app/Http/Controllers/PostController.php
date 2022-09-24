9c390daa567f9f05b81ed7dc40107e721b3eeeed
Teddy Firman <teddyfirman902@gmail.com>
2022-09-24 22:02:52 +0700

    done searching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,24 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
+        // Untuk searching dibawah ini
+        // $posts = Post::latest();
 
-        if(request('search')) {
-            $posts->where('title', 'like', '%' . request('search') . '%');
+        // if(request('search')) {
+        //     $posts->where('title', 'like', '%' . request('search') . '%')->orWhere('body', 'like', '%' . request('search') . '%');
+        // }
+
+        $title = '';
+        if(request('category')){
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+
+        }
+
+        if(request('author')){
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+
         }
 
         return view('posts', [
-            'title' => 'All Posts',
+            'title' => 'All Posts' . $title,
             'active' => 'blog',
             // "posts" => Post::all()
-            'posts' => $posts->get()
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
         ]);
     }
 

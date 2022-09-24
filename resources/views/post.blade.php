5ed8ee237f9c280d5a6dfddd1fc34355d7290a78
Teddy Firman <teddyfirman902@gmail.com>
2022-09-24 12:26:23 +0700

    redesign done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,9 +2,9 @@
 
 @section('container')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mb-5">
             <div class="col-md-8">
-                <h2>{{ $post->title }}</h2>
+                <h2 class="mb-3">{{ $post->title }}</h2>
 
                 <p>By. <a href="/authors/{{ $post->author->username }}"
                         class="text-decoration-none">{{ $post->author->name }}</a> in
@@ -12,7 +12,14 @@
                         {{ $post->category->name }}</a>
                 </p>
 
-                {!! $post->body !!}
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}"
+                    class="img-fluid">
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+
+
 
 
                 <a href="/blog" class="d-block mt-3">Back to Posts</a>

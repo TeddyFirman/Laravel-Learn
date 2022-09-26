d55e4f415e4abeb246b60edad746f657d78c5a6b
Teddy Firman <teddyfirman902@gmail.com>
2022-09-26 13:56:48 +0700

    done user login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,13 +11,29 @@
                 </div>
             @endif
 
+            @if (session()->has('loginError'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('loginError') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
             <h1 class="h3 mb-3 font-weight-normal text-center">Please login</h1>
-            <form>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control mt-2" placeholder="Email address" required
-                    autofocus>
-                <label for="inputPassword" class="sr-only mt-3">Password</label>
-                <input type="password" id="inputPassword" class="form-control mt-2" placeholder="Password" required>
+            <form action="/login" method="post">
+                @csrf
+                <label for="email" class="sr-only">Email address</label>
+                <input type="email" name="email" id="email"
+                    class="form-control mt-2 @error('email') is-invalid @enderror" placeholder="Email address" required
+                    autofocus value="{{ old('email') }}">
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <label for="password" class="sr-only mt-3">Password</label>
+
+                <input type="password" name="password" id="password" class="form-control mt-2" placeholder="Password"
+                    required>
                 <button class="w-100 btn btn-lg btn-primary btn-block mt-3" type="submit">Login</button>
 
 

e9e281e55fe9468586973e05d2b710ef7f87322f
Teddy Firman <teddyfirman902@gmail.com>
2022-09-27 10:43:50 +0700

    validation & create done

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,12 +6,13 @@
             <div class="col-lg-8">
                 <h2 class="mb-3">{{ $post->title }}</h2>
 
-                <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
+                <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my
+                    posts</a>
                 <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
                 <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
 
-                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}"
-                    class="img-fluid mt-3">
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
+                    alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
@@ -20,7 +21,6 @@
 
 
 
-                <a href="/blog" class="d-block mt-3">Back to Posts</a>
             </div>
         </div>
     </div>
